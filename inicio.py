de70ccf41fcc8105237be367b358e4e6ed206ce9
JeronimoVasquez <jevasquez@unal.edu.co>
2023-10-30 00:04:11 -0500

    d

--- inicio.py
+++ inicio.py
@@ -11,12 +11,12 @@ st.set_page_config(
 
 def iniciar_variables():
     if 'count' not in st.session_state:
-        st.session_state['count'] = 0
+        st.session_state.count = 0
 
-    if st.session_state['count'] == 0:
-        st.session_state['logged_in'] = False
+    if st.session_state.count == 0:
+        st.session_state.logged_in= False
 
-    st.session_state['count'] += 1
+    st.session_state.count += 1
 
 iniciar_variables()
 
